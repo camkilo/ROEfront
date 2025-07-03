864a1734b05c711353ee4a187f98c101a8c1bb09
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-03 16:16:10 -0400

    Update page.tsx

--- app/blueprints/page.tsx
+++ app/blueprints/page.tsx
@@ -1,9 +1,12 @@
 import BlueprintPanel from "@/components/Blueprints/BlueprintPanel";
+import AppShell from "@/components/Layout/AppShell";
 
 export default function BlueprintPage() {
   return (
     <main className="min-h-screen p-10 bg-gradient-to-br from-black to-emerald-900">
+       <AppShell>
       <BlueprintPanel />
     </main>
+      </AppShell>
   );
 }
