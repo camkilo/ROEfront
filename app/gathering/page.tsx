575a7545b5f57f36834b76db47dc90bfcc98a00f
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-03 16:03:03 -0400

    Create page.tsx

---
+++ app/gathering/page.tsx
@@ -0,0 +1,9 @@
+import GatheringPanel from "@/components/Gathering/GatheringPanel";
+
+export default function GatheringPage() {
+  return (
+    <main className="min-h-screen p-10 bg-gradient-to-br from-black to-zinc-900">
+      <GatheringPanel />
+    </main>
+  );
+}
