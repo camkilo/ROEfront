a695abc76e9e12fb896d5fc7dd49c1fb0c2931eb
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-03 16:10:28 -0400

    Create page.tsx

---
+++ app/inventory/page.tsx
@@ -0,0 +1,10 @@
+import AppShell from "@/components/Layout/AppShell";
+import InventoryPanel from "@/components/Inventory/InventoryPanel";
+
+export default function InventoryPage() {
+  return (
+    <AppShell>
+      <InventoryPanel />
+    </AppShell>
+  );
+}
