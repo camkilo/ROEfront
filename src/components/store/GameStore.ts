484c19676e2b954afa47d323c62c1f8bea3789c5
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-03 15:49:06 -0400

    Update GameStore.ts

--- src/components/store/GameStore.ts
+++ src/components/store/GameStore.ts
@@ -1,17 +1,27 @@
 import { create } from "zustand";
 
 type GameState = {
-  player: string;
+  player: string | null;
   inventory: string[];
   knownItems: string[];
+  xp: number;
+  level: number;
+  setPlayer: (player: string) => void;
   setInventory: (items: string[]) => void;
   setKnownItems: (items: string[]) => void;
+  setXP: (xp: number) => void;
+  setLevel: (lvl: number) => void;
 };
 
 export const useGameStore = create<GameState>((set) => ({
-  player: "",
+  player: null,
   inventory: [],
   knownItems: [],
+  xp: 0,
+  level: 1,
+  setPlayer: (player) => set({ player }),
   setInventory: (items) => set({ inventory: items }),
   setKnownItems: (items) => set({ knownItems: items }),
+  setXP: (xp) => set({ xp }),
+  setLevel: (level) => set({ level }),
 }));
