2464ed2e498d917d7fca65b10278b45908dff90b
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-03 16:12:51 -0400

    Update page.tsx

--- app/crafting/page.tsx
+++ app/crafting/page.tsx
@@ -1,9 +1,13 @@
 import CraftingPanel from "@/components/Crafting/CraftingPanel";
-
+import AppShell from "@/components/Layout/AppShell";
+  );
+}
 export default function CraftingPage() {
   return (
     <main className="min-h-screen p-10 bg-gradient-to-br from-zinc-900 to-zinc-800">
+      <App Shell/>
       <CraftingPanel />
+            <App Shell/>
     </main>
   );
 }
