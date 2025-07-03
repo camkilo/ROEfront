fa871d7b1781d6b04ca16e670d6786c7724ef1a1
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-03 16:15:01 -0400

    Update page.tsx

--- app/crafting/page.tsx
+++ app/crafting/page.tsx
@@ -5,9 +5,9 @@ import AppShell from "@/components/Layout/AppShell";
 export default function CraftingPage() {
   return (
     <main className="min-h-screen p-10 bg-gradient-to-br from-zinc-900 to-zinc-800">
-      <App Shell/>
+      <App Shell>
       <CraftingPanel />
-            <App Shell/>
-    </main>
+     </main>
+            </App Shell>
   );
 }
