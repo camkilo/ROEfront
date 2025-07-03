760053d3933e12380409b8434cb46ee5ea380f6a
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-03 16:04:43 -0400

    Create page.tsx

---
+++ app/world/page.tsx
@@ -0,0 +1,9 @@
+import WorldMapPanel from "@/components/World/WorldMapPanel";
+
+export default function WorldPage() {
+  return (
+    <main className="min-h-screen p-10 bg-gradient-to-br from-black to-indigo-900">
+      <WorldMapPanel />
+    </main>
+  );
+}
