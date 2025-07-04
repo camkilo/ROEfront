7f45e594ebe662cb4d7145779ceec8d0f7c8cd02
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 12:24:16 -0400

    Update page.tsx

--- app/gathering/page.tsx
+++ app/gathering/page.tsx
@@ -3,10 +3,10 @@ import AppShell from "../../src/components/Layout/AppShell";
 
 export default function GatheringPage() {
   return (
-    <main className="min-h-screen p-10 bg-gradient-to-br from-black to-zinc-900">
-         <AppShell>
-      <GatheringPanel />
-    </main>
-        </AppShell>
+    <AppShell>
+      <main className="min-h-screen p-10 bg-gradient-to-br from-black to-zinc-900">
+        <GatheringPanel />
+      </main>
+    </AppShell>
   );
 }
