b6573ab75c3f0d27ef6e6fa07a4301e4cb8c91ea
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 10:31:00 -0400

    Update page.tsx

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,5 +1,5 @@
-import AppShell from "@/components/Layout/AppShell";
-import InventoryPanel from "@/components/Inventory/InventoryPanel";
+import AppShell from "../components/Layout/AppShell";
+import InventoryPanel from "../components/Inventory/InventoryPanel";
 
 export default function InventoryPage() {
   return (
