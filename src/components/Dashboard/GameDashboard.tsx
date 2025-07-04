47e14af506c869cbd71ea6d57b28cdd4fa922a7a
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 12:56:54 -0400

    Update GameDashboard.tsx

--- src/components/Dashboard/GameDashboard.tsx
+++ src/components/Dashboard/GameDashboard.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import XPDisplay from "@/components/XPDisplay";
-import InventoryPanel from "@/components/InventoryPanel";
-import GatheringPanel from "@/components/GatheringPanel";
-import CraftingPanel from "@/components/CraftingPanel";
-import BlueprintPanel from "@/components/BlueprintPanel";
-import { useGameStore } from "@/store/gameStore";
+import XPDisplay from "../components/XPDisplay";
+import InventoryPanel from "../components/InventoryPanel";
+import GatheringPanel from "../components/Gathering/GatheringPanel";
+import CraftingPanel from "../components/Crafting/CraftingPanel";
+import BlueprintPanel from "../components/Blueprints/BlueprintPanel";
+import { useGameStore } from "../../components/store/gameStore";
 
 export default function GameDashboard() {
   const player = useGameStore((s) => s.player);
