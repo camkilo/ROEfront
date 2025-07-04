3f202080db7c6a8800c5745dc4511a836b3c8ba6
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 15:11:55 -0400

    Update page.tsx

--- app/gathering/page.tsx
+++ app/gathering/page.tsx
@@ -3,12 +3,12 @@ import AppShell from "../../src/components/Layout/AppShell";
 import { useGameStore } from "../../src/components/store/GameStore";
 
 export default function GatheringPage() {
-  
-const player = useGameStore((state) => state.player);  
+  const player = useGameStore((state) => state.player);
+
   return (
     <AppShell>
       <main className="min-h-screen p-10 bg-gradient-to-br from-black to-zinc-900">
-        <GatheringPanel />
+        <GatheringPanel player={player} />
       </main>
     </AppShell>
   );
