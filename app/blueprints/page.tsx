c8e8c8d4a41dccab901668e0d50cd793a160fd2c
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 16:17:56 -0400

    Update page.tsx

--- app/blueprints/page.tsx
+++ app/blueprints/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import BlueprintPanel from "../../src/components/Blueprints/BlueprintPanel";
 import AppShell from "../../src/components/Layout/AppShell";
+import { useGameStore } from "../../src/store/usePlayerStore";
 
 export default function BlueprintPage() {
+  const player = useGameStore((s) => s.player);
+
   return (
     <AppShell>
       <main className="min-h-screen p-10 bg-gradient-to-br from-black to-emerald-900">
-        <BlueprintPanel />
+        <BlueprintPanel player={player} />
       </main>
     </AppShell>
   );
