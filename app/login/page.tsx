fcfeebb150e0035d5696856c2262d53e9d9ffafc
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 19:00:14 -0400

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 // /app/login/page.tsx
 "use client";
-import LoginPage from "../../src/screens/LoginPage";
+import LoginPage from "../../src/screens/Page";
 
 export default function LoginRoute() {
   return <LoginPage />;
