8a625aecc27438a51737ae5a43834429666ce42b
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 17:15:06 -0400

    Update index.ts

--- src/screens/crafting/index.ts
+++ src/screens/crafting/index.ts
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import api from "../../lib/api/Client";
 import { usePlayerStore } from "../../store/usePlayerStore";
-import { Button } from "../../components/ui/Button";
+import { Button } from "../../components/ui/button";
 import { Card } from "../../components/ui/Card";
 import { Modal } from "../../components/ui/Modal";
 
