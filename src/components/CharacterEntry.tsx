bd416b29b4cd11bd911c794009108e683104ca07
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 13:17:39 -0400

    Update CharacterEntry.tsx

--- src/components/CharacterEntry.tsx
+++ src/components/CharacterEntry.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { useGameStore } from "../store/gameStore";
+import { useGameStore } from "./store/gameStore";
 import {
   fetchInventory,
   fetchXP,
