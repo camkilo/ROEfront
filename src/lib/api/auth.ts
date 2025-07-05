c76047713f55cda4cb087ecf76d2a19be1f8f037
camkilo <60801206+camkilo@users.noreply.github.com>
2025-07-04 21:09:07 -0400

    Update auth.ts

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,1 +1,16 @@
+import api from "../lib/api/Client";
+import { loadPlayerState } from "../lib/playerState";
 
+export async function loginUser(username: string, password: string) {
+  const res = await api.post("/login", { username, password });
+  loadPlayerState(res.data);
+  localStorage.setItem("playerName", res.data.player);
+  return res.data;
+}
+
+export async function registerUser(username: string, password: string) {
+  const res = await api.post("/register", { username, password });
+  loadPlayerState(res.data);
+  localStorage.setItem("playerName", res.data.player);
+  return res.data;
+}
