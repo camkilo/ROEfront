0022ebcc62dd0dfeebe5b55a9c941a3bdd95066b
camkilo <60801206+camkilo@users.noreply.github.com>
2025-11-11 10:13:39 -0600

    Update page.tsx

--- app/gathering/page.tsx
+++ app/gathering/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import GatheringPanel from "@/components/Gathering/GatheringPanel";
 import AppShell from "@/components/Layout/AppShell";
-import { useGameStore } from "@/components/store/useGameStore";
+import { useGameStore } from "@/components/store/GameStore";
 
 export default function GatheringPage() {
   const player = useGameStore((state) => state.player);
